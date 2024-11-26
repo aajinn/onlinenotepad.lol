e0d24b302c8504fd8da6ba0aba0846d32448523f
ARed99 <chandyajin@gmail.com>
2024-11-26 10:43:03 +0530

    added about , terms.. , privacy pages

---
+++ app/terms-conditions/page.tsx
@@ -0,0 +1,71 @@
+export default function TermsAndConditions() {
+    return (
+        <div className="container text-2xl p-10 break-words">
+            <h1 className="text-3xl">
+                --Terms and Conditions--
+            </h1>
+            <p>
+                Welcome to
+                OnlineNotepad.lol By
+                accessing or using our
+                application, you agree
+                to comply with and be
+                bound by these terms and
+                conditions.
+            </p>
+
+            <h2>
+                --Use of the Service--
+            </h2>
+            <p>
+                Online Notepad is
+                provided for your
+                personal use and is
+                intended for note-taking
+                purposes. You may not
+                use our service for any
+                illegal or unauthorized
+                purpose.
+            </p>
+
+            <h2>
+                --No Personal
+                Information Collection--
+            </h2>
+            <p>
+                We do not collect any
+                personal information
+                from users, and
+                therefore, we are not
+                liable for any data
+                security concerns
+                regarding user
+                information.
+            </p>
+
+            <h2>
+                --Changes to Terms--
+            </h2>
+            <p>
+                We reserve the right to
+                modify these terms at
+                any time. Any changes
+                will be effective
+                immediately upon posting
+                on this page.
+            </p>
+
+            <h2>--Contact Us--</h2>
+            <p>
+                If you have any
+                questions about these
+                Terms and Conditions,
+                please contact us at{' '}
+                <a href="mailto:careerajin@gmail.com">
+                    careerajin@gmail.com
+                </a>
+                .
+            </p>
+        </div>
+    );
+}
